c6825812877b6592580c6e20e50a2a2d7020f233
Imam Fahrurrozi <imamfa@outlook.com>
2025-05-20 21:54:37 +0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,8 +1,7 @@
-<script>
   document.addEventListener("DOMContentLoaded", function() {
-    const playButton = document.getElementById("playMusic");
-    playButton.addEventListener("click", () => {
-      document.getElementById("bgm").play();
-    });
+     const playButton = document.getElementById("playMusic");
+     playButton.addEventListener("click", () => {
+       document.getElementById("bgm").play();
+     });
   });
-</script>
+
